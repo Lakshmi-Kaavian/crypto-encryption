e4911d64b421d049e38f0d0726a94e51eb7b7e9e
Lakshmi <thangalakshmi.r@kaaviansys.com>
2022-12-01 09:55:06 +0530

    encryption using crypto

---
+++ index.js
@@ -0,0 +1,19 @@
+const crypto = require("crypto");
+const algorithm = "aes-256-cbc"; 
+const initVector = crypto.randomBytes(16);
+const Securitykey = crypto.randomBytes(32);
+console.log(Securitykey);
+
+function encryption (str){
+const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
+let encryptedData = cipher.update(str, "utf-8", "hex");
+encryptedData += cipher.final("hex");
+return encryptedData;
+}
+
+function decryption (str1){
+const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
+let decryptedData = decipher.update(str1, "hex", "utf-8");
+decryptedData += decipher.final("utf8");
+return decryptedData;
+}
